ad55b7ec47643cc33d9562aa73c5bd60bd75a6e4
Rupa Samodder <60574215+rsamodder@users.noreply.github.com>
2024-12-11 11:07:53 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "my_blog");
+define("TITLE", "Travel Guide Barishal");
+define("KEYWORDS", "CSS, PHP, JAVA, JAVA PROGRAMMING, result");
